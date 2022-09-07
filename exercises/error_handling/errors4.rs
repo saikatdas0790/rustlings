3efd4a738dc2003bd34e3647f6a6891cf1b41340
Saikat Das <saikatdas0790@gmail.com>
2022-09-07 16:12:42 +0530

    54/94

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -12,12 +12,18 @@ enum CreationError {
 
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
-        if value < 0 {
-            Err(CreationError::Negative)
-        } else if value == 0 {
-            Err(CreationError::Zero)
-        } else {
-            Ok(PositiveNonzeroInteger(value as u64))
+        // if value < 0 {
+        //     Err(CreationError::Negative)
+        // } else if value == 0 {
+        //     Err(CreationError::Zero)
+        // } else {
+        //     Ok(PositiveNonzeroInteger(value as u64))
+        // }
+
+        match value {
+            x if x < 0 => Err(CreationError::Negative),
+            x if x == 0 => Err(CreationError::Zero),
+            x => Ok(PositiveNonzeroInteger(x as u64)),
         }
     }
 }
